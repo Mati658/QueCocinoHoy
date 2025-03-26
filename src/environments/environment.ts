ab236a439541d6d52dd883de576743f42d4b5bb7
Mati658 <matiasaltamira@outlook.com>
2025-03-25 23:35:35 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoY3J2eWFndmJlY3poamF5cGRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDMwODcsImV4cCI6MjA1NjA3OTA4N30.5JZ4HRoaHcg19P3eRYzeZLEcbOZgiWzAwuyXoy3m9zw',
   SUPABASE_URL : 'https://hhcrvyagvbeczhjaypdb.supabase.co',
   GOOGLE_ID : '478024230940-td8ltuk0kklc6tf12b0f2ita13qpom14.apps.googleusercontent.com',
+  
 };
